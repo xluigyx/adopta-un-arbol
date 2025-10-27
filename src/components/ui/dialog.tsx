672ab52745d671dc7273ab139385a125ad38ed09
xluigyx <rvl5000341@est.univalle.edu>
2025-10-27 01:39:07 -0400

    gaaaa

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import * as React from "react";
-import * as DialogPrimitive from "@radix-ui/react-dialog@1.1.6";
-import { XIcon } from "lucide-react@0.487.0";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { XIcon } from "lucide-react";
+
 
 import { cn } from "./utils";
 
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import * as React from "react";
-import * as SelectPrimitive from "@radix-ui/react-select@2.1.6";
-import {
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
-} from "lucide-react@0.487.0";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+
 
 import { cn } from "./utils";
 
